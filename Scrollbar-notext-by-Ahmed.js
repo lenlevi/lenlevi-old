2bc6efc06e63247910a3f12d9239dfb15ee859e9
lenlevi <100407155+lenlevi@users.noreply.github.com>
2022-07-13 13:20:53 +0200

    Create Scrollbar-notext-by-Ahmed.js

---
+++ Scrollbar-notext-by-Ahmed.js
@@ -0,0 +1,6 @@
+document.addEventListener("scroll", function() {  
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("pbar").style.width = scrolled + "%";
+}); 
